913dd48a3d69ed5d4f446daf167008e18ab46f02
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-10-08 02:27:42 +0500

    'day-1'

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I really love coding...");
+            Console.WriteLine("I like Coding 3 4 5 6");
+            Console.Beep();
+
+        }
+    }
+}
---
+++ class0/class0/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Class0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine('Hell World!');
+        }
+    }
+}
